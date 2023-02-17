3c3cd8058a353bef0b95c3ce5cb1deb8f512cea8
Alex <alex@gmail.com>
2023-02-17 15:16:42 +0530

    missing value found

--- preprocessing.py
+++ preprocessing.py
@@ -1,7 +1,11 @@
+import re
+
 import pandas as pd
 import numpy as np
 import os
 from import_file import data_cleaning
+
+from sklearn.model_selection import train_test_split
 
 
 class preprocessing:
@@ -17,25 +21,49 @@ class preprocessing:
                 #print(self.df.dtypes)
                 float_vals = self.df.select_dtypes(include=['float'])
                 int_vals = self.df.select_dtypes(include=['int'])
-                #print(int_vals.values)
+                #print(int_vals)
                 return int_vals, float_vals
             return
         except Exception as e:
             print("Error occurred in find_datatype function", e)
 
-    def splitting_data(self):
-        int_vals, float_vals = self.find_datatype()
+    def missing_value_data(self):
+        #int_vals, float_vals = self.find_datatype()
         try:
-            null_columns = self.df.columns[self.df.isna().any()]
-            for i in null_columns:
-                sum_of_missing_values =  self.df[i].isna().sum()
-                print("NO OF MISSING VALUES : ", sum_of_missing_values)
+            self.null_columns = self.df.columns[self.df.isna().any()]
+            for i in self.null_columns:
+                sum_of_missing_values = self.df[i].isna().sum()
+                #print("NO OF MISSING VALUES : ", sum_of_missing_values)
                 print("No of missing values in '{}' : ".format(i), self.df[i].isna().sum())
         except Exception as e:
-            print("SPLITTING DATA FUNCTION", e)
+            print("Missing value function", e)
+
+    def splitting_data(self):
+        try:
+            int_vals, float_vals = self.find_datatype()
+            target = []
+            for cols in self.null_columns:
+                target.append(cols)
+                print("TARGET : ", target)
+                '''x = self.df.drop(self.df[target], axis=1)
+                y = self.df[target]
+                missing_x_train, missing_x_test, missing_y_train, missing_y_test = train_test_split(x, y)
+                print("X_TRAIN : \n", missing_x_train)
+                print("X_TEST : \n", missing_x_test)
+                print("Y_TRAIN : \n", missing_y_train)
+                print("Y_TEST : \n", missing_y_test)'''
+                target = []
+        except Exception as e:
+            print("Splittin data function", e)
+
+
+
+
+
 
 
 
 pre = preprocessing()
+pre.missing_value_data()
 pre.splitting_data()
 #pre.filling_missing_values()
