0c37fc3bc0a3a9879bd164db148a2ca4ba122791
Alex <alex@gmail.com>
2023-03-19 01:48:57 +0530

    completed

--- auto_ml_cli.py
+++ auto_ml_cli.py
@@ -1,10 +1,14 @@
 from testing import testing
+import time
 
 
 class the_end:
     def __init__(self):
+        start = time.time()
         test = testing()
         over = test.testing()
+        end = time.time()
+        print("------%s seconds------ " %(round(end-start, 2)))
 
 
 end = the_end()
