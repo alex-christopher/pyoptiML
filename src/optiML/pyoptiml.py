dd200fd7f49e8df0b35b0593b44ab7cd712c9fbf
Alex <alex@gmail.com>
2023-04-04 16:53:52 +0530

    dir changes

---
+++ src/optiML/pyoptiml.py
@@ -0,0 +1,13 @@
+from testing import testing_module
+import time
+
+
+class pyoptiML:
+    def __init__(self):
+        start = time.time()
+        test = testing_module()
+        over = test.testing_user_input()
+        end = time.time()
+        print("------%s seconds------ " % (round(end - start, 2)))
+
+ml = pyoptiML()
